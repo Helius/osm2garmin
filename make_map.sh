fb5de45ad5c7c19466a5a531ba9447d41b861feb
eugene <ghelius@gmail.com>
2012-05-30 10:44:16 +0700

    add options for make_map, now there is different targets, build all new nsk map, rebuld only map, build small test region, for test style

--- make_map.sh
+++ make_map.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+HL="[33m"
+CL="[0m"
+
 usage() {
-	  echo "Usage: $0 [ rebuild_map | update_all ]"
+	  echo $HL"Usage: $0 [ update_all | rebuild_map | test_style ]"$CL
+		echo $HL"    update_all  - download new data and rebuild all"$CL
+		echo $HL"    rebuild_map - rebuild only nsk map with exist contours"$CL
+		echo $HL"    test_style  - like 'rebuild_map' but for small region (it's faster!)"$CL
 		  exit
 }
 
@@ -13,9 +19,11 @@ fi
 
 
 RECT="81.8,54.1,84.5,55.5"
+RECT_TEST="84.0,55.2,84.5,55.5"
 
 if [ "$1" == "update_all" ]; then
 
+	echo $HL"========== update all ============="$CL
 	# get contour line and make OSM file
 	mono Srtm2Osm/Srtm2Osm.exe -bounds3 "http://www.openstreetmap.org/?lat=54.758&lon=83.102&zoom=9&layers=M" -cat 250 50 -step 10 -o no.srtm.osm
 	# make garmin *.img from OSM contour map
@@ -30,6 +38,27 @@ if [ "$1" == "update_all" ]; then
 	bunzip2 -d RU-NVS.osm.bz2
 	# get nsk region from big nso map
 	./osmconvert32 RU-NVS.osm -b=$RECT >nsk.osm
+elif [ "$1" == "test_style" ]; then
+echo $HL"========== test style ============="$CL
+	if [ -f RU-NVS.osm ]; then
+		echo $HL"osm data exist:             ok"$CL
+	else
+		echo $HL"osm data exist:             no"$CL
+		if [ -f RU-NVS.osm.bz2 ]; then
+			echo $HL"bz2 archive of osm data exist: ok"$CL
+		else
+			echo $HL"bz2 archive of osm data exist: no"$CL
+			echo $HL"downloading..."$CL
+			wget http://data.gis-lab.info/osm_dump/dump/latest/RU-NVS.osm.bz2
+		fi
+		bunzip2 -d RU-NVS.osm.bz2
+	fi
+	echo $HL"get small test region"$CL
+	./osmconvert32 RU-NVS.osm -b=$RECT_TEST >nsk.osm
+elif [ "$1" == "rebuild_map" ]; then
+	echo $HL"========== rebuild map  ============="$CL
+	echo $HL"get nsk map region from big map"$CL
+	./osmconvert32 RU-NVS.osm -b=$RECT_TEST >nsk.osm
 fi
 
 # make garmin *.img from main maps 
